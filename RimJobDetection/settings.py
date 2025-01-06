e9ba44c8083e7f3833cc7d1c4d131571d9972a70
Dimitar Mladenov <miti_01@abv.bg>
2025-01-06 17:42:07 +0200

    security changes

--- RimJobDetection/settings.py
+++ RimJobDetection/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -7,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x^pt*3+0-15odc)ks76*-j1j()8r(ww_a%yotch(clulj!vh*f'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
