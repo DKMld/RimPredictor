fa428496844833f9f5e3615fa63d627cff0df265
Dimitar Mladenov <miti_01@abv.bg>
2025-01-08 23:40:25 +0200

    quick bug fix where you can save the same picture but with a different user

--- RimJobDetection/predictions/models.py
+++ RimJobDetection/predictions/models.py
@@ -17,21 +17,12 @@ class UserSearchHistory(models.Model):
     user = models.ForeignKey('accounts.Users', on_delete=models.CASCADE)
     image = models.ImageField(upload_to=os.path.join('static/', settings.MEDIA_URL))
     model_prediction = models.CharField(max_length=64, null=True)
-    image_hash = models.CharField(max_length=128, unique=True, null=True)
+    image_hash = models.CharField(max_length=128, unique=False, null=True)
 
     @staticmethod
     def calculate_image_hash(image):
         hasher = hashlib.md5()
-
         for chunk in image.chunks():
             hasher.update(chunk)
         return hasher.hexdigest()
-
-    # def save(self, *args, **kwargs):
-    #     if self.image:
-    #         self.image_hash = self.calculate_image_hash()
-    #         if UserSearchHistory.objects.filter(image_hash=self.image_hash).exists():
-    #             print('exists')
-    #             return
-    #     super().save(*args, **kwargs)
 
